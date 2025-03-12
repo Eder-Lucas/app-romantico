6bd91984b066ad53ef0c5d72e5885ff166b882ce
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-03-12 17:09:21 -0300

    adicionei o favicon e fiz algumas mudanças

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('install', function (event) {
                 './manifest.json',
                 './index.js',
                 './script.js',
-                'style.css',
+                './style.css',
                 './images'
             ])
         })
